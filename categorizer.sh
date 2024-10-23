4c3d58a2e6320a865a6b27f9c72b1aecf6f7bb19
vivy13 <n.vivy13@gmail.com>
2024-10-24 00:35:51 +0900

    Add installation instructions and execution steps

---
+++ categorizer.sh
@@ -0,0 +1,126 @@
+#!/bin/bash
+
+# ---------- 固定値 ----------
+# ファイル格納先,マウント先
+MOUNT_PATH="$1"
+MOUNT_TARGET_PATH="$2"
+
+
+# 拡張子
+IMAGE_EXTs=("JPG" "jpg" "PNG" "png" "GIF" "gif")
+VIDEO_EXTs=("MP4" "mp4" "MOV" "mov")
+
+# フォルダ
+SAVE_DIR="save"
+SYNC_DIR="sync"
+
+CATEGORY_DIR="year"
+IMAGE_DIR="image"
+VIDEO_DIR="video"
+
+
+
+# ---------- メソッド ----------
+function GetRecordYearByImage () {
+    RecordDatetime=`identify -verbose "$1" | grep DateTime: | awk '{print $2}' | awk -F ':' '{print $1}'`
+    echo $RecordDatetime
+}
+
+function GetRecordYearByVideo () {
+    RecordDatetime=`ffmpeg -i "$1" 2>&1 | grep -E 'creation_time' | head -1 | awk -F ': ' '{print $2}' | awk -F '-' '{print $1}'`
+    echo $RecordDatetime
+}
+
+function IsImageExt() {
+    local ret=0
+    local ith
+    for ith in ${IMAGE_EXTs[@]}; do
+        if [[ ${ith} = ${1} ]]; then
+            local ret=1
+        fi
+    done
+    echo $ret
+}
+
+function IsVideoExt() {
+    local ret=0
+    local ith
+    for ith in ${VIDEO_EXTs[@]}; do
+        if [[ ${ith} = ${1} ]]; then
+            local ret=1
+        fi
+    done
+    echo $ret
+}
+
+
+
+# ---------- 実処理 ----------
+# if [[ ! -d $MOUNT_PATH ]]; then
+#     sudo mkdir $MOUNT_PATH
+# fi
+
+# sudo mount -t drvfs -o metadata,uid=1000,gid=1000 $MOUNT_TARGET_PATH $MOUNT_PATH
+cd $MOUNT_PATH
+
+echo '処理ファイル一覧取得中'
+files=`find ./${SYNC_DIR} -maxdepth 9 -type f -print0 | xargs -0 ls`
+
+# for ithfile in $files;
+# do
+while IFS= read -r ithfile;
+do
+    ithfile="${ithfile}"
+    echo $ithfile
+    # read -p "Press [Enter] key to resume."
+
+    ext="${ithfile##*.}"
+    isImage=$(IsImageExt $ext)
+
+    targetCategory=""
+    if [[ 1 == $isImage ]]; then
+        targetCategory=$IMAGE_DIR
+
+    elif [[ 1 != $isImage ]]; then
+        isVideo=$(IsVideoExt $ext)
+
+        if [ 1 == $isVideo ]; then
+            targetCategory=$VIDEO_DIR
+        fi
+    else
+        # 画像・動画でない場合スキップ
+        continue
+    fi
+
+    year="unknown"
+    if [[ $IMAGE_DIR == $targetCategory ]]; then
+        year=$(GetRecordYearByImage "$ithfile")
+
+    elif [[ $VIDEO_DIR == $targetCategory ]]; then
+        year=$(GetRecordYearByVideo "$ithfile")
+    fi
+
+    # 撮影日時の情報がない場合
+    if [[ -z $year ]]; then
+        # continue
+        year="unknown"
+    fi
+
+    # 格納先がなければ作成
+    targetPath=${MOUNT_PATH}/${SAVE_DIR}/${CATEGORY_DIR}/${year}/${targetCategory}
+    if [[ ! -d $targetPath ]]; then
+        sudo mkdir -p $targetPath
+    fi
+
+    fname=`basename "$ithfile"`
+    mv -f "$ithfile" ${targetPath}/"${fname}"
+
+    # 異常がなければ削除
+    if [[ -z $ERRORLEVEL ]]; then
+        echo "完了"
+    else
+        ERRORLEVEL=
+        continue
+    fi
+# done
+done <<< "$files"
---
+++ executer.bat
@@ -0,0 +1,7 @@
+@echo off
+echo;
+setlocal enabledelayedexpansion
+@REM Bash categorizer.sh /mnt/nas //tosix-NAS/Multimedia/tosix/media
+Bash categorizer.sh /mnt/z/temp/multimedia/tosix13/
+
+pause
